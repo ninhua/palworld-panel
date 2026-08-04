47f96cdf4ff20a769cd45a5f5c55d903037d4a19
JunWei Li <mailninhua@163.com>
2026-08-04 23:56:33 +0800

    sync contracts.ts patchVersion to 0.8.83

--- frontend/src/api/generated/contracts.ts
+++ frontend/src/api/generated/contracts.ts
@@ -1510,7 +1510,7 @@ export interface components {
       "patch": {
         "features": Array<string>;
         "repository": "ninhua/palworld-panel";
-        "version": "0.8.82";
+        "version": "0.8.83";
       };
       "upstream": {
         "commit": string;
